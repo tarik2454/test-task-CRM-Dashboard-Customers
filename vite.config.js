847d2956d7f8a25fbe52949aed3fcc2e0e71b259
Taras Kovalyk <tarik2454@gmail.com>
2024-01-06 00:12:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // base: '/test-task-CRM-Dashboard-Customers/',
+  base: '/test-task-CRM-Dashboard-Customers/',
 });
