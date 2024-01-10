3e20a1a6b1e35328f0b57be398090c074486bf0f
Taras Kovalyk <tarik2454@gmail.com>
2024-01-10 16:41:16 +0200

    adds changes vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: '/test-task-CRM-Dashboard-Customers/',
 });
