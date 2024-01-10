36b5cd7b8d9997fcfd36a153e684192db03ac080
Taras Kovalyk <tarik2454@gmail.com>
2024-01-10 16:26:25 +0200

    adds versel.json

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
-
-import react from '@vitejs/plugin-react-swc';
-import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  css: {
-    devSourcemap: true,
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/test-task-CRM-Dashboard-Customers/',
 });
